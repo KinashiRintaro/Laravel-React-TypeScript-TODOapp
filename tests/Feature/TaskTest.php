46f4111a00242c4790366e02386b9173f41a6d7d
KinashiRintaro <kinashi_r@mediaxis.jp>
2023-04-30 12:24:33 +0900

    タスクのCRUD APIを作成

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -23,4 +23,47 @@ class TaskTest extends TestCase
             ->assertOk()
             ->assertJsonCount($tasks->count());
     }
+
+    /**
+     * @test
+     */
+    public function タスクを登録することができる(): void
+    {
+        $data = [
+            'title' => 'テスト投稿'
+        ];
+        $response = $this->postJson('api/tasks', $data);
+
+        $response
+            ->assertCreated()
+            ->assertJsonFragment($data);
+    }
+    
+    /**
+     * @test
+     */
+    public function タスクを更新することができる(): void
+    {
+        $task = Task::factory()->create();
+        $task->title = '書き換え';
+
+        $response = $this->patchJson("api/tasks/{$task->id}", $task->toArray());
+        $response
+            ->assertOk()
+            ->assertJsonFragment($task->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function タスクを削除することができる(): void
+    {
+        $task = Task::factory()->count(10)->create();
+        $task->title = '書き換え';
+
+        $response = $this->deleteJson("api/tasks/1");
+        $response->assertOk();
+        $response = $this->getJson("api/tasks");
+        $response->assertJsonCount($task->count() - 1);
+    }
 }
