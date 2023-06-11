0080d1cf34c10709a2b3a8cbd27aec53a59e37d0
KinashiRintaro <kinashi_r@mediaxis.jp>
2023-06-11 17:15:27 +0900

    ReactQueryインストール

--- resources/ts/components/page/index.tsx
+++ resources/ts/components/page/index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import Router from "../Router/router";
+import {QueryClient, QueryClientProvider} from "react-query";
 
 function App() {
-    return <Router />;
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                // エラー時に何回取得するか（今回は取得しない）
+                retry: false
+            }, 
+            mutations: {
+                retry: false
+            }
+        }
+    });
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Router />
+        </QueryClientProvider>
+    )
 }
 
 export default App;
